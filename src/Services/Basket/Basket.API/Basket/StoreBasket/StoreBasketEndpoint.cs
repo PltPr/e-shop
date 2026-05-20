98715640bfc539738a7fd691c36b60fbe74358f1
etzy1337 <etzy@onet.pl>
2026-05-20 16:25:37 +0200

    get,store basket

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Basket.API.Basket.StoreBasket
+{
+	public record StoreBasketRequest(ShoppingCart Cart);
+	public record StoreBasketResponse(bool IsSuccess);
+	public class StoreBasketEndpoint : ICarterModule
+	{
+		public void AddRoutes(IEndpointRouteBuilder app)
+		{
+			app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+			{
+				var command = request.Adapt<StoreBasketCommand>();
+
+				var result = sender.Send(command);
+
+				var response = result.Adapt<StoreBasketResponse>();
+
+				return Results.Ok(response);
+			})
+				.WithName("StoreBasket")
+				.Produces<StoreBasketResponse>(StatusCodes.Status201Created)
+				.ProducesProblem(StatusCodes.Status400BadRequest)
+				.WithSummary("Create Basket")
+				.WithDescription("Create Basket");
+		}
+	}
+}
---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Basket.API.Basket.StoreBasket
+{
+	public record StoreBasketCommand(ShoppingCart Cart) :ICommand<StoreBasketResult>;
+	public record StoreBasketResult(bool isSuccess);
+
+	public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
+	{
+		public StoreBasketCommandValidator()
+		{
+			RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+			RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
+		}
+	}
+	public class StoreBasketHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+	{
+		public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+		{
+			ShoppingCart cart = command.Cart;
+
+			return new StoreBasketResult(true);
+		}
+	}
+}
